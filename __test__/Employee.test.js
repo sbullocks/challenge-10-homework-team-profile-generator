5ae5122786de1881479b7d65d4090d2b9259c251
sbullocks <sbullocks@gmail.com>
2022-10-25 18:41:12 -0400

    added test for employee

---
+++ __test__/Employee.test.js
@@ -0,0 +1,65 @@
+const { it } = require('node:test');
+const { describe } = require('yargs');
+const Employee = require('../lib/employee.classes');
+
+describe('Employee', () => {
+    describe('constructor', () => {
+        it('should return an object with three values when called with new keyword', () => {
+            const obj = new Employee();
+            
+            expect('name' in obj).toEqual(true);
+            expect('id' in obj).toEqual(true);
+            expect('email' in obj).toEqual(true);
+        });
+        it('shoul set name, id, and email when created', () => {
+            const name = 'Stephen';
+            const id = '123';
+            const email = 'sbullocks@gmail.com';
+
+            const obj = new Employee(name, id, email);
+
+            expect(obj.name).toEqual(name);
+            expect(obj.id).toEqual(id);
+            expect(obj.email).toEqual(email);
+        });
+    });
+    describe('getName', () => {
+        it('should return employee name', () => {
+            const name = 'Stephen';
+
+            const newName = Employee(name, '123', 'sbullocks@gmail.com').getName();
+
+            expect(name).toEqual(newName);
+        });
+    });
+
+    describe('getId', () => {
+        it('should return an employee id', () => {
+            const id ='123';
+
+            const newId = new Employee('Stephen', id, 'sbullocks@gmail.com').getId();
+
+            expect(id).toEqual(newId);
+        });
+    });
+
+    describe('getEmail', () => {
+        it('should return an employee email', () => {
+            const id ='sbullocks@gmail.com';
+
+            const newEmail = new Employee('Stephen', '123', email).getEmail();
+
+            expect(id).toEqual(newEmail);
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return an employee role', () => {
+            const id ='Employee';
+
+            const newRole = new Employee('Stephen', '123', 'sbullocks@gmail.com').getRole();
+
+            expect(id).toEqual(newRole);
+        });
+    });
+})
